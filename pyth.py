8d894914d580100b93a135875811fc006f13c35d
Sosna Gemechu <sosna.gemechu@aastustudent.edu.et>
2025-06-16 13:27:24 +0300

    Update pyth.py

--- pyth.py
+++ pyth.py
@@ -17,9 +17,24 @@ def login():
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        email = request.form['email']
+        name = request.form['name']
+        age = request.form['age']
+        nationality = request.form['nationality']
+        residence = request.form['residence']
+        arrival_day = request.form['arrival_day']
+        username = request.form['username']
         password = request.form['password']
-        return f"<h1>Thanks for signing up, {email}!</h1>"
+
+        # Here you could store data in a database or a file
+
+        return f"""
+            <h1>Thanks for signing up, {name}!</h1>
+            <p>Nationality: {nationality}</p>
+            <p>Residence: {residence}</p>
+            <p>Arrival Date: {arrival_day}</p>
+            <p>Your username is: {username}</p>
+            <a href='/login'>Go to login</a>
+        """
     return render_template('signup.html')
 
 @app.route('/north-shewa')
